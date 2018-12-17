9e8dad4e9eccb62a24cd43ab85915e94b9e375aa
Bryce Blankenfeld <36483751+brazzblank@users.noreply.github.com>
2018-12-16 19:01:00 -0600

    Update Roman_Numeral_Converter.js

--- JavaScript_Algorithms_DataStructures/Roman_Numeral_Converter.js
+++ JavaScript_Algorithms_DataStructures/Roman_Numeral_Converter.js
@@ -1,10 +1,12 @@
 function convertToRoman(num) {
-  var lookup = {M:1000,CM:900,D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1},roman = '',i;
-  for ( i in lookup ) {
-    while ( num >= lookup[i] ) {
+  var romanlibrary = {M:1000, CM:900, D:500, CD:400, C:100, XC:90, L:50, XL:40, X:10, IX:9, V:5, IV:4, I:1}, roman = '',i;
+  for (i in romanlibrary) {
+    while (num >= romanlibrary[i]) {
       roman += i;
-      num -= lookup[i];
+      num -= romanlibrary[i];
     }
   }
   return roman;
 }
+
+convertToRoman(36);
